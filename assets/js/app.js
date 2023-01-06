9dc07b60384cd864b3f5263b7e7a3897f238ab5f
Esteban <cddanguez2001@gmail.com>
2023-01-05 23:01:09 -0600

    desarrollo de añadir evento y borrarlos con clic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,31 @@
+const formulario = document.getElementById("evento");
+const fecha = document.getElementById("date");
+const btnenviar = document.getElementById("btnEnviar");
+const vacio = document.getElementById("nada");
+const nuevo = document.createElement("p");
+const button = document.createElement('button');
 
-const formulario = document.getElementById('evento');
-const btnenviar = document.getElementById('btnEnviar');
+btnenviar.addEventListener("click", anadir);
 
-
-btnenviar.addEventListener('click', () =>{
+function anadir() {
   event.preventDefault();
-  
-console.log('first')
+      nuevo.innerHTML = formulario.value;
+      const aqui = document.getElementById("aquiso");
 
-  alert(`hola ${formulario.value}`);
-  form.reset();
-})
+      aqui.appendChild(nuevo); /*Esto lo hicimo para mete a button dentro de p*/
+      nuevo.appendChild(button);
+      vacio.style.display = "none";
+      form.reset();
+};
 
+function boton (){
+
+
+}
+
+
+  button.addEventListener('click',()=>{
+    console.log('hola')
+    const quitar = document.getElementById('aquiso');
+    quitar.removeChild(nuevo);
+  });
